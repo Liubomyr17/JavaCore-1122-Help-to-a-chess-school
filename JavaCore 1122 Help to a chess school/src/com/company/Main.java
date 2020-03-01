6f311132762d704ca51d93fd9069ba9c93f40cc5
Code First! <41821541+Liubomyr17@users.noreply.github.com>
2020-03-01 22:05:35 +0200

    Add files via upload

---
+++ JavaCore 1122 Help to a chess school/src/com/company/Main.java
@@ -0,0 +1,46 @@
+package com.company;
+
+/*
+
+1122 Help to a chess school
+Add a common ChessFigure base class to the shape classes: (chess pieces).
+Requirements:
+1. Add the public chess class ChessFigure to the Solution class.
+2. The King class must be inherited from the ChessFigure class.
+3. The Queen class must be inherited from the ChessFigure class.
+4. The Rook class must be inherited from the ChessFigure class.
+5. The Knight class must be inherited from the ChessFigure class.
+6. The Bishop class must be inherited from the ChessFigure class.
+7. The Pawn class must be inherited from the ChessFigure class.
+
+ */
+
+import java.io.*;
+import java.lang.reflect.Array;
+import java.net.URISyntaxException;
+import java.nio.file.FileSystemException;
+import java.nio.file.attribute.AclFileAttributeView;
+import java.rmi.RemoteException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+    }
+    public class ChessFigure {
+    }
+    public class King extends ChessFigure {
+    }
+    public class Queen extends ChessFigure {
+    }
+    public class Rook extends ChessFigure {
+    }
+    public class Knight extends ChessFigure {
+}
+public class Bishop extends ChessFigure {
+}
+
+public class Pawn extends ChessFigure {
+}
+}
